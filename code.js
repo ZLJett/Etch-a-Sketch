69c802034e8832d057901c933229d8dca0f1d383
ZLJett <oneredraven@gmail.com>
2022-06-30 14:52:38 -0700

    Test taking input from dialog box and having it change page

--- code.js
+++ code.js
@@ -1,11 +1,13 @@
 const sizeDialog = document.querySelector(".size-dialog");
 const sizeButton = document.querySelector(".change-size");
-const closeButton = document.querySelector(".close-button");
+const enterButton = document.querySelector(".enter-button");
+const screen = document.querySelector(".screen");
 
 sizeButton.addEventListener("click", () => {
   sizeDialog.showModal();
 })
 
-closeButton.addEventListener("click", () => {
+enterButton.addEventListener("click", () => {
+  screen.textContent = "hello";
   sizeDialog.close();
 })
