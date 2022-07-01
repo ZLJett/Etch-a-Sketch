30c90ee9f91e6bab9360d7d19341d742f7e49a40
ZLJett <oneredraven@gmail.com>
2022-07-01 16:24:22 -0700

    Replace Change Color button with more important Reset button and add functionality to it

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ const sizeDialog = document.querySelector(".size-dialog");
 const changeSize = document.querySelector(".change-size");
 const enterSize = document.querySelector(".enter-size");
 const screen = document.querySelector(".screen");
+const resetScreen = document.querySelector(".reset-screen");
 let isDrawing = false;
 
 function removeCells() {
@@ -16,10 +17,10 @@ function addCells(size) {
     newCell.classList.add("cell");
     // Prevent div from being dragged instead of "colored"
     newCell.addEventListener('dragstart', (event) => {
-      event.preventDefault()
+      event.preventDefault();
     });
     newCell.addEventListener('drop', (event) => {
-      event.preventDefault()
+      event.preventDefault();
     });
     // Color first cell on click then all subsequently moused over cells
     newCell.addEventListener("mousedown", () => {
@@ -48,9 +49,14 @@ function initialSetup(event) {
   addCells(startingCells);
 }
 
+function resetCells(event) {
+  const cellList = document.querySelectorAll(".cell");
+  cellList.forEach(cell => cell.style.background = "none");
+}
+
 window.addEventListener("load", initialSetup);
 
-// Can only draw while mouse is held down and on screen 
+// Can only draw while mouse is held down and on screen
 screen.addEventListener("mousedown", () => {
   isDrawing = true;
 });
@@ -65,6 +71,8 @@ changeSize.addEventListener("click", () => {
 });
 
 enterSize.addEventListener("click", populateScreen);
+
+resetScreen.addEventListener("click", resetCells);
 
 
 
